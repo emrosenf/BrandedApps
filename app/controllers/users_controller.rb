d7fe3ac85295e204a99abc192bfcc72135616e42
Evan Rosenfeld <evan.m.rosenfeld@gmail.com>
2012-01-18 10:46:44 -0800

    Fix logical bug in account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,15 @@ class UsersController < ApplicationController
     required.each{|r| render_404 unless params.has_key? r}
     app = App.find(params[:app_id]) || render_404
     @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to apps_url, :notice => "Logged in!"
-    else
-      flash[:error] = "Invalid email or password"
-      redirect_to login_path
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to apps_url, :notice => "Logged in!" and return
+      else
+        flash[:error] = "Invalid email or password"
+        redirect_to login_path and return
+      end
     end
-    
     @user = User.new(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
     @user.status = 1
     if @user.save
